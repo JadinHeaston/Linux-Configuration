dbd93e04370d989ee5b5c2c05e03d3a7bad3d461
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2023-12-11 19:01:44 -0600

    Doing work to the webdav secret setting.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -94,16 +94,18 @@ if [ "${INSTALL_KEEPASSXC,}" == "true" ]; then
 
 	sudo apt install -y davfs2
 	#sudo dpkg-reconfigure davfs2
-	mkdir -p $KEEPASS_WEBDAV_MOUNT_PATH
-	touch ~/.davfs2/secrets
-	echo "$KEEPASS_WEBDAV_URL	$KEEPASS_WEBDAV_USERNAME	$KEEPASS_WEBDAV_PASSWORD" > ~/.davfs2/secrets #Tab delimited (# act as comments, escape them)
-	chmod 600 ~/.davfs2/secrets
-	echo "$KEEPASS_WEBDAV_URL $KEEPASS_WEBDAV_MOUNT_PATH davfs user,_netdev,auto,file_mode=600,dir_mode=700 0 1" > /etc/fstab
+	sudo mkdir -p "$KEEPASS_WEBDAV_MOUNT_PATH"
+	sudo mkdir ~/.davfs2
+	sudo touch ~/.davfs2/secrets
+	sudo chown $USER:$USER -R ~/.davfs2
+	echo "$KEEPASS_WEBDAV_URL	$KEEPASS_WEBDAV_USERNAME	$KEEPASS_WEBDAV_PASSWORD" >> ~/.davfs2/secrets #Tab delimited (# act as comments, escape them)
+	sudo chmod 600 ~/.davfs2/secrets
+	sudo su -c "echo \"$KEEPASS_WEBDAV_URL \"$KEEPASS_WEBDAV_MOUNT_PATH\" davfs user,_netdev,auto,file_mode=600,dir_mode=700 0 1\" >> /etc/fstab"
 	sudo usermod -a -G davfs2 $USER
-	touch ~/.bash_profile
 	sudo systemctl enable NetworkManager-wait-online.service #Disable for a potential boot improvement.
-	echo "mount $KEEPASS_WEBDAV_MOUNT_PATH" > ~/.bash_profile #Running on login.
-	mount $KEEPASS_WEBDAV_MOUNT_PATH
+	touch ~/.bash_profile
+	echo "mount \"$KEEPASS_WEBDAV_MOUNT_PATH\"" > ~/.bash_profile #Running on login.
+	mount "$KEEPASS_WEBDAV_MOUNT_PATH"
 fi
 
 #### Thunderbird
